8241eb2040d1d12ae0745c3968ecc2ed51f01ee7
vladimirovicp <vladimirovicp70@gmail.com>
2022-11-11 16:07:48 +0400

    add: page group

--- public_html/modules/custom/ssu_schedule/src/Controller/GroupShowController.php
+++ public_html/modules/custom/ssu_schedule/src/Controller/GroupShowController.php
@@ -1,6 +1,8 @@
 <?php
 namespace Drupal\ssu_schedule\Controller;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Routing\TrustedRedirectResponse;
+use Drupal\Core\Url;
 
 
 class GroupShowController extends ControllerBase {
@@ -22,20 +24,78 @@ class GroupShowController extends ControllerBase {
     $query -> orderBy('number','ASC');
     $group = $query->execute()->fetchAll();
 
+    $education_count = '';
+    $type_count = '';
+    $course_count = '';
+    $group_count = '';
+
+    $group_format = [];
+
+    $education_info = [
+      0 => [
+        'title'=> 'Дневная форма обучения',
+        'alias'=> 'do'
+      ],
+      1 => [
+        'title'=> 'Заочная форма обучения',
+        'alias'=> 'zo'
+      ],
+      2 => [
+        'title'=> 'Вечерняя форма обучения',
+        'alias'=> 'vo'
+      ]
+    ];
+
+    $education_info_type = [
+      0 => 'Специалитет',
+      1 => 'Бакалавриат',
+      2 => 'Магистратура',
+      3 => 'Аспирантура',
+    ];
+
+
+    for ($i = 0; $i< count($group); $i++){
+      if($group[$i]->form_education != $education_count){
+        $education_count = $group[$i]->form_education;
+        $group_format[$education_count]['title'] = $education_info[$education_count]['title'];
+        $group_format[$education_count]['alias'] = $education_info[$education_count]['alias'];
+        $type_count = '';
+      }
+
+      if($group[$i]->grp_type != $type_count){
+        $type_count = $group[$i]->grp_type;
+        $group_format[$education_count]['info'][$type_count]['title'] = $education_info_type[$type_count];
+
+      }
+
+      if( floor(intval($group[$i]->number)/100)%10 != $course_count){
+        $course_count = floor(intval($group[$i]->number)/100)%10;
+        $group_format[$education_count]['info'][$type_count]['info'][$course_count]['name'] = $course_count;
+        $group_count = 0;
+      }
+      $group_format[$education_count]['info'][$type_count]['info'][$course_count]['group'][$group_count] = $group[$i]->number;
+      $group_count++;
+
+    }
+
+    if (empty($group)){
+      $response = new TrustedRedirectResponse(
+        Url::fromUri('internal:/schedule')->toString()
+      );
+      $response->send();
+    }
+
     return array(
       '#theme' => 'schedule_group',
-      '#group' => $group,
+      '#faculty' => $param,
+      '#group' => $group_format,
     );
-
-
 
   }
 
 
   public function titleCallback($param) {
-
     return  $param;
-
   }
 
 }
